eabac24b5d7238b4b1fef8244f4f4c9737401ad4
iTacey <tacey_wong@163.com>
2014-09-14 21:36:20 +0800

    Create Color_Reverse.c

---
+++ Color_Reverse.c
@@ -0,0 +1,15 @@
+//对图片进行反色处理
+
+
+
+for(int i=1;i<height;i++)
+{
+    for(int j=1;j<width;j++)
+    {
+        output_r[i][j]=255-r[i][j];
+        output_g[i][j]=255-g[i][j];
+        output_b[i][j]=255-b[i][j];
+    }
+}
+    
+    
