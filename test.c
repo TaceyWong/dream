b07e323f366455a2d6d1821a3c29e33689dc92f1
iTacey <tacey_wong@163.com>
2014-09-14 21:42:24 +0800

    Create test.c

---
+++ test.c
@@ -0,0 +1,10 @@
+//浮雕效果
+for(int i=1;i<height;i++)
+{
+    for(int j=1;j<width;j++)
+    {
+        output_r[i][j]=r[i-1][j-1]-r[i+1][j+1]+128 ;
+        output_g[i][j]=g[i-1][j-1]-g[i+1][j+1]+128 ;
+        output_b[i][j]=b[i-1][j-1]-b[i+1][j+1]+128 ;
+    }
+}
