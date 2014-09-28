5d1498aff0f7c22bacc2d5d9ce60045b2a934e7a
Tacey Wong <tacey_wong@163.com>
2014-09-28 14:39:05 +0800

    添加一个效果代码

--- MatPic.cpp
+++ MatPic.cpp
@@ -194,3 +194,10 @@ float y=(j+sin((i*i+_sq(j-700)*5)/100./DIM)*35)*s;
 return (int(29*((i+DIM)*s+y))%2+int(29*((DIM*2-i)*s+y))%2)*127;
 }
 
+/Pie  :D
+/* RED */
+    int a=(j?i%j:i)*4;int b=i-32;int c=j-32;return _sq(abs(i-512))+_sq(abs(j-512))>_sq(384)?a:int(sqrt((b+c)/2))^_cb((b-c)*2);
+/* GREEN */
+    int a=(j?i%j:i)*4;return _sq(abs(i-512))+_sq(abs(j-512))>_sq(384)?a:int(sqrt((i+j)/2))^_cb((i-j)*2);
+/* BLUE */
+    int a=(j?i%j:i)*4;int b=i+32;int c=j+32;return _sq(abs(i-512))+_sq(abs(j-512))>_sq(384)?a:int(sqrt((b+c)/2))^_cb((b-c)*2);
